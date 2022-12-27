6b8e6472965576c04fe445e6fb1e6489bcbd8901
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-12-27 11:20:21 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//func that takes 3params
+
+function employee(name, height, country) {
+  alert(`My name is ${name}, I am ${height}ft tall and I live in ${country}.`);
+}
+
+employee("Scotty", " 6.1 ", "United States");
